7092e5522bf4fa92686ccdabbda44077de3e0427
Saurav Singh <161109610+sauravsingh3021@users.noreply.github.com>
2024-11-14 04:20:29 +0530

    Update Reservation.jsx

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://restaurant-application-t4ln.onrender.com/api/v1/reservation/send",
+        "http://localhost:4000/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
